628959358651899cdd3d6177fc52429b409329b5
KenWood2b <kenwood2b@gmail.com>
2024-11-05 19:10:26 +0300

    First

--- ArraysConsoleApp/ArraysConsoleApp/Program.cs
+++ ArraysConsoleApp/ArraysConsoleApp/Program.cs
@@ -6,13 +6,13 @@ class Program
     {
         Random random = new Random();
 
-       
+        // Запрашиваем у пользователя количество строк и столбцов для матрицы
         Console.WriteLine("Введите количество строк:");
         int rows = int.Parse(Console.ReadLine());
         Console.WriteLine("Введите количество столбцов:");
         int columns = int.Parse(Console.ReadLine());
 
-        
+        // Создаем матрицу и переменную для хранения суммы элементов
         int[,] matrix = new int[rows, columns];
         int sum = 0;
 
@@ -21,14 +21,14 @@ class Program
         {
             for (int j = 0; j < columns; j++)
             {
-                matrix[i, j] = random.Next(-10, 11); 
+                matrix[i, j] = random.Next(-10, 11);  // заполняем случайными числами от -10 до 10
                 sum += matrix[i, j];
                 Console.Write(matrix[i, j] + "\t");
             }
             Console.WriteLine();
         }
 
-       
+        // Вывод суммы всех элементов матрицы
         Console.WriteLine($"\nСумма всех элементов матрицы: {sum}");
         Console.ReadKey();
     }
--- ArraysConsoleApp/Exercise2ConsoleApp/Program.cs
+++ ArraysConsoleApp/Exercise2ConsoleApp/Program.cs
@@ -10,19 +10,24 @@ namespace Exercise2ConsoleApp
     {
         static void Main(string[] args)
         {
+            // Размер матриц
             int rows = 2;
             int columns = 3;
 
+            // Объявление и инициализация матриц A и B
             int[,] matrixA = { { 2, -3, 1 }, { 5, 4, -2 } };
             int[,] matrixB = { { 4, 2, -5 }, { -4, 1, 3 } };
             int[,] matrixSum = new int[rows, columns];
 
+            // Вывод матрицы A
             Console.WriteLine("Матрица A:");
             PrintMatrix(matrixA, rows, columns);
 
+            // Вывод матрицы B
             Console.WriteLine("\nМатрица B:");
             PrintMatrix(matrixB, rows, columns);
 
+            // Сложение матриц A и B и вывод результата
             Console.WriteLine("\nСумма матриц A и B:");
             for (int i = 0; i < rows; i++)
             {
@@ -34,6 +39,7 @@ namespace Exercise2ConsoleApp
             PrintMatrix(matrixSum, rows, columns);
         }
 
+        // Метод для вывода матрицы на экран
         static void PrintMatrix(int[,] matrix, int rows, int columns)
         {
             for (int i = 0; i < rows; i++)
